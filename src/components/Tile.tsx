a03562639b7cdd03764e4ae9f0e1c1221b21ff1d
Ruslan Ivanov <rusivary@gmail.com>
2024-06-14 13:25:47 +0400

    feature/tile | added styled components in Tile

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components'
 import { TileData } from '../utils/types'
 
 interface TileProps {
@@ -6,13 +7,35 @@ interface TileProps {
 
 const Tile = ({ tile }: TileProps) => {
   return (
-    <div className="tile">
+    <StyledTile>
       <img src={tile.Image} alt={tile.Name} />
       <h3>{tile.Name}</h3>
       <p>{tile.Description}</p>
-      <p>Price: ${tile.Price}</p>
-    </div>
+      <p className="price">Price: ${tile.Price}</p>
+    </StyledTile>
   )
 }
 
 export default Tile
+
+const StyledTile = styled.div`
+  border: 1px solid #ddd;
+  padding: 10px;
+  text-align: center;
+
+  img {
+    width: 100%;
+  }
+
+  h3 {
+    margin: 0.5em 0;
+  }
+
+  p {
+    margin: 0.5em 0;
+  }
+
+  .price {
+    font-weight: bold;
+  }
+`
