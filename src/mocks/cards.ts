c8caf19eecf5041312cef9042dc206ea1c3e1472
Ruslan Ivanov <rusivary@gmail.com>
2024-06-18 18:57:23 +0400

    changed number of tiles

--- src/mocks/cards.ts
+++ src/mocks/cards.ts
@@ -1,10 +1,10 @@
 import { avatar } from '../assets/avatar_image'
 import { TileData } from '../utils/types'
 
-export const exampleTiles: TileData[] = Array(25)
+export const exampleTiles: TileData[] = Array(11)
   .fill(null)
   .map((_, index) => ({
-    id: index + 1,
+    id: (index + 1).toString(),
     name: `Tile ${index + 1}`,
     description: `Description ${index + 1}`,
     image: avatar,
